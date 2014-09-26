4f395caac0b33b03851bf551302b1ed14f68ffc9
Evandro Lima <eslimaf@gmail.com>
2014-09-26 01:22:42 -0300

    Update the API infos for Customer and Technicians

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  before_filter :set_hostname
   protect_from_forgery with: :null_session
+
+  def set_hostname
+  	@hostname = request.host
+  end
 end
