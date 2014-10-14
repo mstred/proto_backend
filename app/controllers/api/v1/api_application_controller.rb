cb0fe85f32713b051a1ee3fcd2c9127b3ea8d617
Evandro Lima Filho <eslimaf@gmail.com>
2014-10-14 19:13:46 -0300

    Verify if access_token is correct

--- app/controllers/api/v1/api_application_controller.rb
+++ app/controllers/api/v1/api_application_controller.rb
@@ -5,16 +5,17 @@ module Api
             before_filter :authenticate_token!
 
             def authenticate_token!
-                token = request.headers['Token']
-                api_consumer_token = ENV['API_TOKEN']
-                if token != api_consumer_token
+                unless check_customer_access_token!
                     render_unauthorized
                 end
             end
 
-            def render_unauthorized
-              self.headers['WWW-Authenticate'] = 'Token realm="Application"'
-              render json: 'Bad credentials', status: 401
+            def check_customer_access_token!
+                customer = Customer.find_by_access_token(request.headers['Token'])
+                if customer != nil
+                    return true
+                end
+                return false
             end
         end
     end
